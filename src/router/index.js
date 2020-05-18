0d94ebbf84db168c21d4ded96704bc4374dfa5df
Professionel <maximebarlet.pro@gmail.com>
2020-05-18 18:31:32 +0200

    initial commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Listing from '../views/Listing.vue'
+import Article from '../views/Article.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,14 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/listing',
+    name: 'Listing',
+    component: Listing
+  },
+  {
+    path: '/article/:slug',
+    name: 'Article',
+    component: Article
   }
 ]
 
---
+++ src/views/Article.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="article">
+    {{ $route.params.slug }}
+  </div>
+</template>
