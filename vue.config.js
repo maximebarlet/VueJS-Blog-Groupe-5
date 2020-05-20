edac0ead2471349cca31475be43686043ce8c70f
Professionel <maximebarlet.pro@gmail.com>
2020-05-20 12:48:04 +0200

    icons fix

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,13 @@ module.exports = {
 		msTileColor: '#000000',
 		appleMobileWebAppCapable: 'yes',
 		appleMobileWebAppStatusBarStyle: 'black',
+		iconPaths: {
+			favicon32: '/public/img/icons/favicon-32x32.png',
+			favicon16: '/public/img/icons/favicon-16x16.png',
+			appleTouchIcon: '/public/img/icons/apple-icon-precomposed.png',
+			maskIcon: '/public/favicon.ico',
+      		msTileImage: '/public/img/icons/android-icon-192x192.png',
+		  },
 		manifestOptions: {
 			display: 'fullscreen'
 		},
